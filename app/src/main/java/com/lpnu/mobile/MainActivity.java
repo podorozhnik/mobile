8d2681ff8eaeda4c013728c34df1420563ffc1c0
Antonina Melnyk <antonina.melnyk99@gmail.com>
2018-09-13 23:05:25 +0300

    add UI for lab3

--- app/src/main/java/com/lpnu/mobile/MainActivity.java
+++ app/src/main/java/com/lpnu/mobile/MainActivity.java
@@ -2,68 +2,46 @@ package com.lpnu.mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    protected EditText first_name;
+    protected EditText last_name;
+    protected EditText email;
+    protected EditText phone;
+    protected EditText password;
+    protected EditText confirm_password;
+    protected Button submit_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello, world!");
+        first_name = findViewById(R.id.firstName);
+        last_name = findViewById(R.id.lastName);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
+        confirm_password = findViewById(R.id.passwordConfirm);
+        submit_button = findViewById(R.id.button);
         onClickButtonsHandler();
     }
 
     public void onClickButtonsHandler(){
-        final Button helloButton = (Button) findViewById(R.id.button);
-        final Button btn_clear = (Button) findViewById(R.id.btn_clear);
-        final EditText nameInput = (EditText) findViewById(R.id.editText);
-        final TextView label = (TextView) findViewById(R.id.textView);
+        submit_button.setOnClickListener(new View.OnClickListener() {
 
-        helloButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nameInput.getText().toString();
-                if (name.matches("")) {
-                    label.setText("Please write your name.");
-                } else {
-                    label.setText("Hello, " + name + "!");
-                }
-                label.setVisibility(View.VISIBLE);
+                first_name.setError("Invalid");
+                last_name.setError("Invalid");
+                email.setError("Invalid");
+                phone.setError("Invalid");
+                password.setError("Invalid");
+                confirm_password.setError("Invalid");
             }
         });
 
-        nameInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.toString().trim().length() == 0) {
-                    btn_clear.setVisibility(View.INVISIBLE);
-                } else {
-                    btn_clear.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-
-        btn_clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nameInput.setText("");
-            }
-        });
     }
 }

37775fd9768a00134642613793057f48e280f2e6
Antonina Melnyk <antonina.melnyk99@gmail.com>
2018-09-13 21:18:29 +0300

    Add lab2

--- app/src/main/java/com/lpnu/mobile/MainActivity.java
+++ app/src/main/java/com/lpnu/mobile/MainActivity.java
@@ -2,6 +2,12 @@ package com.lpnu.mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hello, world!");
+        onClickButtonsHandler();
+    }
+
+    public void onClickButtonsHandler(){
+        final Button helloButton = (Button) findViewById(R.id.button);
+        final Button btn_clear = (Button) findViewById(R.id.btn_clear);
+        final EditText nameInput = (EditText) findViewById(R.id.editText);
+        final TextView label = (TextView) findViewById(R.id.textView);
+
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameInput.getText().toString();
+                if (name.matches("")) {
+                    label.setText("Please write your name.");
+                } else {
+                    label.setText("Hello, " + name + "!");
+                }
+                label.setVisibility(View.VISIBLE);
+            }
+        });
+
+        nameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() == 0) {
+                    btn_clear.setVisibility(View.INVISIBLE);
+                } else {
+                    btn_clear.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        btn_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameInput.setText("");
+            }
+        });
     }
 }

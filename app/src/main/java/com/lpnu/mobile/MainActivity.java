57cbda537a7be5a6538e8c0b6d45e2f297ff80b4
Antonina Melnyk <antonina.melnyk99@gmail.com>
2018-09-14 09:53:19 +0300

    start validations

--- app/src/main/java/com/lpnu/mobile/MainActivity.java
+++ app/src/main/java/com/lpnu/mobile/MainActivity.java
@@ -34,14 +34,21 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                first_name.setError("Invalid");
-                last_name.setError("Invalid");
-                email.setError("Invalid");
-                phone.setError("Invalid");
+                Validator(first_name.getText().toString(), "First Name", first_name, "^[A-Z][a-zA-Z]+$");
+                Validator(last_name.getText().toString(), "Last Name", last_name, "^[A-Z][a-zA-Z]+$");
+                Validator(email.getText().toString(), "Email", email, "^[a-zA-Z0-9+_.-]+@[a-zA-Z]+\\.[A-Za-z]{2,4}$");
+                Validator(phone.getText().toString(), "Phone", phone, "\\+?[0-9]{10,16}");
                 password.setError("Invalid");
                 confirm_password.setError("Invalid");
             }
         });
+    }
 
+    public void Validator(String value, String field, EditText field_id, String regex){
+        if(value.equals("")){
+            field_id.setError(field + " cannot be empty.");
+        } else if(!(value.matches(regex))){
+            field_id.setError("Invalid value of " + field);
+        }
     }
 }

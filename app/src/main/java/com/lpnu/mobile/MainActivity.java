248817cffaf8e9c7b45851f5d18abdb540c7c3ea
Antonina Melnyk <antonina.melnyk99@gmail.com>
2018-10-07 17:28:30 +0300

    display images on ui. lab6

--- app/src/main/java/com/lpnu/mobile/MainActivity.java
+++ app/src/main/java/com/lpnu/mobile/MainActivity.java
@@ -1,17 +1,68 @@
 package com.lpnu.mobile;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-import com.lpnu.mobile.controller.Controller;
+import com.lpnu.mobile.adapters.PixabayAdapter;
+import com.lpnu.mobile.interfaces.PixabayAPI;
+import com.lpnu.mobile.models.Hit;
+import com.lpnu.mobile.models.PhotoList;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.list_photos) RecyclerView recyclerView;
+    private final static String BASE_URL = "https://pixabay.com/api/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Controller controller = new Controller();
-        controller.start();
+        ButterKnife.bind(this);
+        buildRetrofit();
+    }
+
+    public void buildRetrofit(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        PixabayAPI pixabayAPI = retrofit.create(PixabayAPI.class);
+        Call<PhotoList> call = pixabayAPI.getData();
+        call.enqueue(new Callback<PhotoList>() {
+            @Override
+            public void onResponse(@NonNull Call<PhotoList> call, @NonNull Response<PhotoList> response) {
+                Log.d("onResponse", "ServerResponse: " + response.toString());
+                PhotoList photoList = response.body();
+                ArrayList<Hit> hits = Objects.requireNonNull(photoList).getHits();
+                drawData(hits);
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<PhotoList> call, @NonNull Throwable t) {
+                Log.e("onFailure", t.getMessage());
+            }
+        });
+    }
+
+    public void drawData(ArrayList<Hit> data){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        PixabayAdapter adapter = new PixabayAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/lpnu/mobile/adapters/PixabayAdapter.java
@@ -0,0 +1,66 @@
+package com.lpnu.mobile.adapters;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lpnu.mobile.MainActivity;
+import com.lpnu.mobile.R;
+import com.lpnu.mobile.models.Hit;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PixabayAdapter  extends RecyclerView.Adapter<PixabayAdapter.PixabayViewHolder> {
+    ArrayList<Hit> photos;
+    public PixabayAdapter(ArrayList<Hit> photos) {
+        this .photos = photos;
+    }
+
+    @Override
+    public PixabayViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.photo_list_item, parent, false);
+        return new PixabayViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(PixabayViewHolder holder, int position) {
+        holder.tags.setText(photos.get(position).getTags());
+        holder.user.setText(photos.get(position).getUser());
+        Picasso.get().load(photos.get(position).getLargeImageURL()).into(holder.image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return photos.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public class PixabayViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.cv) CardView cardView;
+        @BindView(R.id.tags) TextView tags;
+        @BindView(R.id.user) TextView user;
+        @BindView(R.id.image) ImageView image;
+        public PixabayViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
